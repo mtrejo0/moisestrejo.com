d587f7c4aa69bd79bdf957a87eb7f7525ebf8cf2
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-11-10 18:35:45 -0500

    added logo

--- src/containers/contact.tsx
+++ src/containers/contact.tsx
@@ -1,56 +1,71 @@
+import { Grid, Stack, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 
 import contact from "../information/contact.json";
 
 import links from "../information/links.json";
 
-class Contact extends React.Component {
-  render() {
-    return (
-      <div className="center">
-        <h1 className="title">Lets talk!</h1>
+const Contact = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-        <div className="nice-border">
-          <a
-            href={"https://forms.gle/yJRmC6x1C7oq1sVu8"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Send me a message!
-          </a>
-        </div>
-        <div className="contact-grid">
-          {contact.map((item) => {
-            return (
-              <div className="nice-border">
-                {item.link ? (
-                  <a href={item.link} target="_blank" rel="noopener noreferrer">
-                    {item.name}
-                  </a>
-                ) : (
-                  <p>{item.name}</p>
-                )}
-                <p>{item.description}</p>
-              </div>
-            );
-          })}
-        </div>
-        <h1 className="title">Quick Links</h1>
-        <ul>
-          {links.map((link) =>
-            link.ids.map((id) => {
-              return (
-                <li>
-                  <a href={link.link} target="_blank" rel="noopener noreferrer">
-                    moisestrejo.com/{id}
-                  </a>
-                </li>
-              );
-            })
-          )}
-        </ul>
+  return (
+    <div className="center">
+      <h1 className="title">Lets talk!</h1>
+      <div className="nice-border">
+        <a
+          href={"https://forms.gle/yJRmC6x1C7oq1sVu8"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Send me a message!
+        </a>
       </div>
-    );
-  }
-}
+
+      <Grid container>
+        {contact.map((item) => {
+          const body = (
+            <div className="nice-border">
+              {item.icon ? (
+                <img src={item.icon} style={{ width: "100px" }}></img>
+              ) : (
+                <p>{item.name}</p>
+              )}
+              <p>{item.description}</p>
+            </div>
+          );
+
+          return (
+            <Grid item xs={isMobile ? 6 : 3}>
+              {item.link ? (
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ textDecoration: "none" }}
+                >
+                  {body}
+                </a>
+              ) : (
+                body
+              )}
+            </Grid>
+          );
+        })}
+      </Grid>
+      <h1 className="title">Quick Links</h1>
+      <Stack spacing={2} mb={8}>
+        {links.map((link) =>
+          link.ids.map((id) => {
+            return (
+              <a href={link.link} target="_blank" rel="noopener noreferrer">
+                moisestrejo.com/{id}
+              </a>
+            );
+          })
+        )}
+      </Stack>
+    </div>
+  );
+};
 export default Contact;

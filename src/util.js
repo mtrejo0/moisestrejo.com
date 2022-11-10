6d7c05b320071205ddbcedf06d1416de88915912
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-11-10 16:51:37 -0500

    new

--- src/util.js
+++
@@ -1,8 +0,0 @@
-export function shuffle(array){
-    for(let i = array.length - 1; i > 0; i--){
-        const j = Math.floor(Math.random() * i)
-        const temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-    }
-}

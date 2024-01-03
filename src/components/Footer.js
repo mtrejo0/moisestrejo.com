df38ca806c9f76dbb1c951a7c420224e08a57424
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2024-01-02 21:06:16 -0600

    added fb

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,49 @@
-import React from 'react';
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+
 
 const Footer = () => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
     return (
-        <footer style={styles.footerStyle}>
-            <div style={styles.sidePanelStyle}>
+        <Box 
+            sx={{
+                color: '#000',
+                padding: '32px',
+                borderTop: '1px solid black',
+                marginTop: '128px',
+                marginBottom: '64px',
+                flexDirection: isMobile ? 'column' : 'row', // Stack content on mobile
+                alignItems: 'center', // Center content
+                justifyContent: 'space-evenly', // Align content evenly
+                textAlign: isMobile ? 'center' : 'left', // Center text on mobile
+                display: 'flex'
+            }}
+        >
+            <Box>
                 <p>Contact Me.</p>
                 <a
-                    href={"https://moisestrejo.com/contact"}
-                    style={{ textDecoration: "none", color: "inherit" }}
+                    href="https://moisestrejo.com/contact"
+                    style={{ textDecoration: 'none', color: 'inherit' }}
                     target="_blank"
                     rel="noreferrer"
                 >
                     <b>moisestrejo.com/contact</b>
                 </a>
-                <br></br>
-                
-                <div class="fb-like" 
+                <br />
+                <div className="fb-like" 
                     data-href="https://moisestrejo.com" 
                     data-width="" 
                     data-layout="" 
                     data-action="" 
                     data-size="" 
                     data-share="true"
-                    style={{marginTop: "16px"}}
+                    style={{ marginTop: '16px' }}
                 ></div>
-            </div>
+            </Box>
             <p>© Moises Trejo Industries</p>
-        </footer>
+        </Box>
     );
-};
-
-const styles = {
-    footerStyle: {
-        color: '#000',
-        padding: '32px',
-        left: '0',
-        bottom: '0',
-        display: 'flex',
-        justifyContent: 'space-evenly', // Aligns the content and side panel
-        borderTop: "1px black solid",
-        marginTop: "128px",
-        marginBottom: "64px"
-    },
-    sidePanelStyle: {
-        textAlign: 'left' // Aligns the contact information to the right
-    }
 };
 
 export default Footer;
